35ff313a69974051bbae7b873969ea74807b9353
Marco Giunta <giunta.marco@spes.uniud.it>
2021-06-12 10:53:40 +0200

    Added periodNaive and periodSmart source code files.

---
+++ apps/periodNaive.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 1000   // maximum size of a line of input
+
+/**
+ * Search longest border (naive algorithm)
+ * @param s string
+ * @param n s string length
+ * @return max edge size
+ */
+int searchLongestBorderNaive(char *s, int n) {
+    char prefix[n];
+    char suffix[n];
+    int longestBorderLength = 0;
+
+    int i = 1;
+    while (i < n) {
+        snprintf(prefix, n+1, "%.*s", i, &(s[0]));
+        snprintf(suffix, n+1, "%s", &(s[n - i]));
+
+        /* compare suffix and prefix */
+        if (strcmp(prefix,suffix) == 0) {
+            longestBorderLength = (int) strlen(prefix);
+        }
+        i++;
+
+    }
+    return longestBorderLength;
+}
+
+int main () {
+    char s[MAX_LINE_SIZE];
+    scanf("%[^\n]s", s);
+    int n = strlen(s);
+
+    // border
+    int r = searchLongestBorderNaive(s, n);
+
+    // period
+    int p = n - r;
+
+    printf("%d", p);
+    return 0;
+}
---
+++ apps/periodSmart.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 1000   // maximum size of a line of input
+
+/**
+ * Search longest border (smart algorithm)
+ * @param s string
+ * @param n s string length
+ * @return max border length
+ */
+int searchLongestBorderSmart(const char *s, int n) {
+
+    int r[n];  /* create borders length array */
+    r[0] = 0;  /* border length of first character is always 0 */
+
+    int currentBorderLength = 0;
+    for (ssize_t i = 1; i < n; ++i) {
+        while (currentBorderLength > 0 && s[currentBorderLength] != s[i])
+            currentBorderLength = r[currentBorderLength - 1];
+        if (s[currentBorderLength] == s[i])
+            ++currentBorderLength;
+
+        r[i] = currentBorderLength;
+    }
+    return r[n-1];
+}
+
+int main () {
+    char s[MAX_LINE_SIZE];
+    scanf("%[^\n]s", s);
+    int n = strlen(s);
+
+    // border
+    int r = searchLongestBorderSmart(s, n);
+
+    // period
+    int p = n - r;
+
+    printf("%d", p);
+    return 0;
+}
